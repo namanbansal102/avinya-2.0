537ae8376e6c8b68748e3bd9e3f437d011170f19
Naman Bansal <namanbansal102@gmail.com>
2024-10-21 16:38:06 +0530

    Initial Commit

---
+++ src/app/mypage/page.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import { AnimatedModalDemo } from './AnimatedModalDemo'
+
+const page = () => {
+  return (
+    <div>
+        <AnimatedModalDemo></AnimatedModalDemo></div>
+  )
+}
+
+export default page
---
+++ src/app/ourpage/page.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import {Avatar} from "@nextui-org/react";
+const page = () => {
+  return (
+    <div>
+        
+        <Avatar src="https://i.pravatar.cc/150?u=a042581f4e29026024d" />
+
+    </div>
+  )
+}
+
+export default page

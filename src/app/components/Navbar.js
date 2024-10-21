41121b82b66d2aaf64f7074133405163549ff7ec
Naman Bansal <namanbansal102@gmail.com>
2024-10-21 23:50:56 +0530

    Add Sign Up Page

--- src/app/components/Navbar.js
+++ src/app/components/Navbar.js
@@ -33,7 +33,7 @@ export default function Navbar() {
   }
 
   return (
-    <nav className={`fixed w-full  z-50 mb-10 border-2 border-black   transition-all duration-300 ${isScrolled ? 'bg-white shadow-lg' : 'bg-transparent'}`}>
+    <nav className={`fixed w-full  z-50 mb-10 shadow-lg   transition-all duration-300 ${isScrolled ? 'bg-white shadow-lg' : 'bg-transparent'}`}>
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 ">
         <div className="flex items-center justify-between h-16 ">
           <div className="flex items-center">
